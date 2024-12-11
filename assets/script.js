648a0a10d4a1c1dd1f46869d7c49f8ce0a4d70b7
Olivier Agbala <ronniking99@gmail.com>
2024-12-11 01:05:34 +0100

    travail sur le css

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,19 @@
-   // script.js
-
-   document.addEventListener("DOMContentLoaded", function() {
-    // Sélectionner tous les liens de navigation
+document.addEventListener("DOMContentLoaded", function() {
     const navLinks = document.querySelectorAll('.main-nav a');
-
-    // Ajouter un écouteur d'événements pour chaque lien
+    
     navLinks.forEach(link => {
         link.addEventListener('click', function(event) {
-            event.preventDefault(); // Empêche le comportement par défaut du lien
-
-            // Récupérer l'ID de la section cible
-            const targetId = this.getAttribute('href');
+            event.preventDefault(); 
+            const targetId = this.getAttribute('href'); 
             const targetSection = document.querySelector(targetId);
-
-            console.log(`Naviguer vers : ${targetId}`); // Debug
-
-            // Faire défiler la page vers la section cible
+            
             if (targetSection) {
                 targetSection.scrollIntoView({
-                    behavior: 'smooth', // Défilement fluide
-                    block: 'start' // Aligne le début de la section en haut de la fenêtre
+                    behavior: 'smooth',
+                    block: 'start'
                 });
             } else {
-                console.log(`Section non trouvée : ${targetId}`); // Debug
+                console.error(`Section not found: ${targetId}`);
             }
         });
     });
