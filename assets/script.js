e764d818a884a1f7f64cbb0b1311694398d24b15
Olivier Agbala <ronniking99@gmail.com>
2024-12-11 04:27:07 +0100

    laoding

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     const navLinks = document.querySelectorAll('.main-nav a');
+    const loadingScreen = document.getElementById('loading');
 
+    // Gestion des clics sur les liens de navigation
     navLinks.forEach(link => {
         link.addEventListener('click', function(event) {
             event.preventDefault(); // Empêche le comportement par défaut du lien
             const targetId = this.getAttribute('href'); // Récupère l'ID de la section cible
             
+            // Afficher l'écran de chargement
+            loadingScreen.style.display = 'flex';
+
             // Masquer toutes les sections
             document.querySelectorAll('.section-content').forEach(section => {
                 section.classList.remove('active');
@@ -19,9 +24,21 @@ document.addEventListener("DOMContentLoaded", function() {
                     behavior: 'smooth',
                     block: 'start'
                 });
+
+                // Masquer l'écran de chargement après un court délai
+                setTimeout(() => {
+                    loadingScreen.style.display = 'none';
+                }, 500); // Ajustez le délai selon vos besoins
             } else {
                 console.error(`Section not found: ${targetId}`);
+                loadingScreen.style.display = 'none'; // Masquer l'écran de chargement en cas d'erreur
             }
         });
+    });
+
+    // Gestion de l'envoi du formulaire de newsletter
+    document.getElementById('newsletter-form').addEventListener('submit', function(e) {
+        e.preventDefault();
+        alert('Merci de vous être inscrit à notre newsletter !');
     });
 });
