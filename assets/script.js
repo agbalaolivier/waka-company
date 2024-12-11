8a4a708700e7f3e995171d3004c69500a401bcb5
Olivier Agbala <ronniking99@gmail.com>
2024-12-11 03:39:48 +0100

    avancement et charme

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     const navLinks = document.querySelectorAll('.main-nav a');
-    
+
     navLinks.forEach(link => {
         link.addEventListener('click', function(event) {
-            event.preventDefault(); 
-            const targetId = this.getAttribute('href'); 
-            const targetSection = document.querySelector(targetId);
+            event.preventDefault(); // Empêche le comportement par défaut du lien
+            const targetId = this.getAttribute('href'); // Récupère l'ID de la section cible
             
+            // Masquer toutes les sections
+            document.querySelectorAll('.section-content').forEach(section => {
+                section.classList.remove('active');
+            });
+
+            // Afficher la section cible
+            const targetSection = document.querySelector(targetId);
             if (targetSection) {
+                targetSection.classList.add('active');
                 targetSection.scrollIntoView({
                     behavior: 'smooth',
                     block: 'start'
